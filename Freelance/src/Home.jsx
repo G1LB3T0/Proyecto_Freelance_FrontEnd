267aa38634b8719683a01792b039e7532ef79195
Zacatac23 <zac231104@uvg.edu.gt>
2025-05-08 21:01:14 -0600

    Inicio de Home

---
+++ Freelance/src/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+ const Sidebar = () => (
+  <aside className="sidebar">
+    <h2 className="sidebar-title">📋 Menú</h2>
+    <ul className="sidebar-menu">
+      <li>📅 Calendario</li>
+      <li>💳 Pagos</li>
+      <li>🛠 Proyectos Activos</li>
+      <li>🏠 Home</li>
+      <li>🧑‍💻 Freelancer</li>
+      <li>📂 Proyectos</li>
+    </ul>
+  </aside>
+);

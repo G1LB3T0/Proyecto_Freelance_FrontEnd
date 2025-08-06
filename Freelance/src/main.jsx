fd0577144859b2170bd03f5283e0a0d57369bf80
Zacatac23 <132374201+Zacatac23@users.noreply.github.com>
2025-08-05 20:11:31 -0600

    Update main.jsx

--- Freelance/src/main.jsx
+++ Freelance/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-
+import './pages/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
